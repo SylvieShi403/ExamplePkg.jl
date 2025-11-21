49de58aa1ee9ff484b3476f3c9ee64dca60d1385
“SylvieShi403” <lfs5712@psu.edu>
2025-11-21 13:34:42 -0500

    init code

--- src/ExamplePkg.jl
+++ src/ExamplePkg.jl
@@ -2,4 +2,20 @@ module ExamplePkg
 
 greet() = print("Hello World!")
 
-end # module ExamplePkg
+"Package demonstrating how to make your code into a Julia package"
+
+using Distributions
+
+export pdf_normal
+
+"""
+   pdf_normal(x, mu, sigma)
+
+Return pdf of a normal distribution with mean mu and standard deviation sigma at point x.
+"""
+function pdf_normal(x::Number, mu::Number, sigma::Number)
+  dist = Normal(mu,sigma)
+  pdf(dist,x)
+end
+
+end # module
